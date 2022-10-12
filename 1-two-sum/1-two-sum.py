a8fc46ca34ad9d77ba41c6e07f35439d3010052d
Deep Sanghavi <deepsanghavi1002@gmail.com>
2022-10-12 02:36:44 -0400

    Time: 72 ms (88.19%), Space: 15.2 MB (51.90%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i,v in enumerate(nums):
+          remaining = target - v
+          if remaining in seen:
+            return [seen[remaining], i]
+          seen[v] = i
+        return([])
