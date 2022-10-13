9924ee6cf27a97fca77f3a42caf734447f40d059
Deep Sanghavi <deepsanghavi1002@gmail.com>
2022-10-12 20:54:31 -0400

    Time: 344 ms (24.09%), Space: 16.8 MB (19.25%) - LeetHub

---
+++ 136-single-number/136-single-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        temp = nums[0]
+        for i in range(1,len(nums)):
+            
+            temp = nums[i]^temp
+        return temp
+            
