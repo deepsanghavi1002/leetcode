f63578401a466235a96b308c9558be0e3f8d63d4
Deep Sanghavi <deepsanghavi1002@gmail.com>
2022-10-13 20:06:19 -0400

    Time: 52 ms (70.14%), Space: 13.9 MB (67.26%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.py
+++ 686-repeated-string-match/686-repeated-string-match.py
@@ -1,17 +1,24 @@
-class Solution:
-    def repeatedStringMatch(self, a: str, b: str) -> int:
-        i = 1
-        tempString = a
-        while(True):
-            tempString = i * a
-            if len(tempString) >10000:
-                break
-            if b in tempString:
+# class Solution:
+#     def repeatedStringMatch(self, a: str, b: str) -> int:
+#         i = 1
+#         tempString = a
+#         while(True):
+#             tempString = i * a
+#             if len(tempString) >10000:
+#                 break
+#             if b in tempString:
                 
-                return i
+#                 return i
             
             
-            #print(tempString, i)
-            i += 1
-        print(len(tempString))
-        return - 1
+#             #print(tempString, i)
+#             i += 1
+#         print(len(tempString))
+#         return - 1
+class Solution(object):
+    def repeatedStringMatch(self, A, B):
+        times = -(-len(B) // len(A)) # Equal to ceil(len(b) / len(a))
+        for i in range(2):
+          if B in (A * (times + i)):
+            return times + i
+        return -1
