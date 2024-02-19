db38aba8c934ea5ad2cb92a27b64326f61a2c101
Deep Sanghavi <deepsanghavi1002@gmail.com>
2024-02-19 17:32:36 -0500

    Time: 34 ms (92.35%), Space: 17 MB (65.98%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -7,18 +7,23 @@ class Node:
 """
 
 from typing import Optional
+from collections import deque
 class Solution:
-    def __init__(self):
-        self.visited = {}
+    
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        visited = {}
+        dq = deque()
+        
         if node == None:
             return None
-        if node in self.visited:
-            return self.visited[node]
-        else:
-            clone_node = Node(val = node.val, neighbors = [])
-            self.visited[node] = clone_node
-            if node.neighbors:
-                clone_node.neighbors = [self.cloneGraph(n)  for n in node.neighbors]
-            return clone_node
+        dq.append(node)
+        visited[node] = Node(node.val, [])
+        while dq:
+            currNode = dq.popleft()
+            for neighbor in currNode.neighbors:
+                if neighbor not in visited:
+                    visited[neighbor] = Node(neighbor.val, [])
+                    dq.append(neighbor)
+                visited[currNode].neighbors.append(visited[neighbor])
+        return visited[node]
         
