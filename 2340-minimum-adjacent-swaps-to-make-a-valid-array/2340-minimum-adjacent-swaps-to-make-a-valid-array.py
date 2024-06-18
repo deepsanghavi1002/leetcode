77bcd1a5907074a944e5929da53eeee30adb31db
Deep Sanghavi <deepsanghavi1002@gmail.com>
2024-06-18 01:22:24 -0400

    Time: 415 ms (5.11%), Space: 30 MB (93.48%) - LeetHub

--- 2340-minimum-adjacent-swaps-to-make-a-valid-array/2340-minimum-adjacent-swaps-to-make-a-valid-array.py
+++ 2340-minimum-adjacent-swaps-to-make-a-valid-array/2340-minimum-adjacent-swaps-to-make-a-valid-array.py
@@ -7,7 +7,7 @@ class Solution:
         
         small_index = nums.index(small)
         large_index = nums[::-1].index(large)
-        print(small_index, large_index)
+       
         
         if small_index>= len(nums) - large_index:
             return small_index + large_index -1
