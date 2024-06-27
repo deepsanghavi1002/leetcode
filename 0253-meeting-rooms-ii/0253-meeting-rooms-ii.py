44362a4110565510b34ccc8a874120fa128a9f4d
Deep Sanghavi <deepsanghavi1002@gmail.com>
2024-06-26 22:50:46 -0400

    Time: 493 ms (5.04%), Space: 27.3 MB (5.09%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -12,7 +12,7 @@ class Solution:
         for i in range(len(start)):
             dp[start[i]] += 1
             dp[end[i]] -= 1
-        print(dp)   
+
         rooms = 0
         res = 0
         for i in dp:
