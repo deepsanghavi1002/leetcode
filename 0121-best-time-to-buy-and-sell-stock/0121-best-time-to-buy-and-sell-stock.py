b1114916a4ac8d1c44900d196a995b5212ddba6a
Deep Sanghavi <deepsanghavi1002@gmail.com>
2024-07-05 16:39:50 -0400

    Time: 820 ms (8.92%), Space: 28.5 MB (6.55%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,9 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         diff = [prices[i+1] - prices[i] for i in range(len(prices) - 1)]
-        
-        
-
         curr_max = 0
         maxx = 0
         for c in diff:
